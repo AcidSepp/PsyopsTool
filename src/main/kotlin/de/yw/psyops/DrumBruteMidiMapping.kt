1029b9e9cb25cc45a27c539b14bc0593b18142fe
yannick <yannickweber95@gmail.com>
2024-10-16 18:46:27 +0200

    add DrumBruteMidiMapping

---
+++ src/main/kotlin/de/yw/psyops/DrumBruteMidiMapping.kt
@@ -0,0 +1,12 @@
+package de.yw.psyops
+
+const val KICK = 36
+const val SNARE_1 = 37
+const val SNARE_2 = 38
+const val TOM_HI = 39
+const val TOM_LOW = 40
+const val CYM = 41
+const val COW = 42
+const val CLOSED_HAT = 43
+const val OPEN_HAT = 44
+const val FM_DRUMS = 45
