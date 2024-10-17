5ddd5b63738cfe8681e3079929e68b0b9d88ab28
yannick <yannickweber95@gmail.com>
2024-10-17 15:39:59 +0200

    wip#1 kts support

---
+++ src/main/kotlin/de/yw/psyops/ScriptHost.kt
@@ -0,0 +1,35 @@
+package de.yw.psyops
+
+import java.io.File
+import kotlin.script.experimental.api.ScriptDiagnostic
+import kotlin.script.experimental.api.constructorArgs
+import kotlin.script.experimental.host.toScriptSource
+import kotlin.script.experimental.jvm.dependenciesFromCurrentContext
+import kotlin.script.experimental.jvm.jvm
+import kotlin.script.experimental.jvmhost.BasicJvmScriptingHost
+import kotlin.script.experimental.jvmhost.createJvmCompilationConfigurationFromTemplate
+import kotlin.script.experimental.jvmhost.createJvmEvaluationConfigurationFromTemplate
+
+fun loadLoopsFromScript(): List<MidiLoop> {
+    val scriptFile =
+        File("/home/yannick/IdeaProjects/PsyopsTool/src/main/resources/dnb.kts")
+    val compilationConfiguration = createJvmCompilationConfigurationFromTemplate<SimpleScript> {
+        jvm {
+            dependenciesFromCurrentContext(wholeClasspath = true)
+        }
+    }
+    val pluginContextImpl = PluginContextImpl()
+    val evaluationConfiguration = createJvmEvaluationConfigurationFromTemplate<SimpleScript> {
+        constructorArgs.put(listOf(pluginContextImpl))
+    }
+    val res =
+        BasicJvmScriptingHost().eval(scriptFile.toScriptSource(), compilationConfiguration, evaluationConfiguration)
+    res.reports.forEach {
+        if (it.severity > ScriptDiagnostic.Severity.DEBUG) {
+            println(" : ${it.message}" + if (it.exception == null) "" else ": ${it.exception}")
+        }
+    }
+    return pluginContextImpl.midiLoops
+}
+
+data class PluginContextImpl(override var midiLoops: MutableList<MidiLoop> = mutableListOf()) : PluginContext
---
+++ src/main/resources/dnb.kts
@@ -0,0 +1,29 @@
+import de.yw.psyops.*
+
+midiLoops = mutableListOf(
+    fillSteps(
+        floatArrayOf(1.0f, 0.25f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.25f),
+        8,
+        KICK
+    ),
+    fillSteps(
+        floatArrayOf(0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.25f),
+        8,
+        SNARE_1
+    ),
+    fillSteps(
+        floatArrayOf(0.0f, 0.0f, 0.1f, 0.1f, 0.0f, 0.0f, 0.1f, 0.1f),
+        8,
+        SNARE_2
+    ),
+    fillSteps(
+        floatArrayOf(0.3f, 0.0f, 0.3f, 0.0f, 0.3f, 0.0f, 0.3f, 0.0f),
+        8,
+        TOM_HI
+    ),
+    fillSteps(
+        floatArrayOf(0.0f, 0.3f, 0.0f, 0.3f, 0.0f, 0.3f, 0.0f, 0.3f),
+        8,
+        FM_DRUMS
+    ),
+)
